2741e777ecc9d5a0430a7de4dd0605f94b56dea0
Adam Mayer <me@phooky.name>
2014-06-02 15:49:48 -0400

    folding simh.py into objdumper

--- objdumper.py
+++ objdumper.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 
-from simh import SIMH
 from pdpobj import ObjFile
 from emulator import Emulator
 import serial
 import argparse
+
+class SIMH:
+    def __init__(self,pipe):
+        self.f = pipe
+        self.addr = 0
+        
+    def load_address(self,address):
+        self.addr = address
+
+    def deposit_word(self, w):
+        self.f.write("d {0} {1}\n".format(oct(self.addr),oct(w)))
+        self.addr = self.addr + 2
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
